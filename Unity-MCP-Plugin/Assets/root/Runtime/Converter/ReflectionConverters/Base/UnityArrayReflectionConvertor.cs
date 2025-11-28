e1c910f8d3d67af1a2dcb3364a4959ff1394440a
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-11-27 21:11:58 -0800

    Enhance JSON converters to support AssetPath and AssetGuid properties; improve logging in UnityEngine_GameObject_ReflectionConvertor

--- Unity-MCP-Plugin/Assets/root/Runtime/Converter/ReflectionConverters/Base/UnityArrayReflectionConvertor.cs
+++ Unity-MCP-Plugin/Assets/root/Runtime/Converter/ReflectionConverters/Base/UnityArrayReflectionConvertor.cs
@@ -13,8 +13,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Text.Json;
 using com.IvanMurzak.ReflectorNet;
 using com.IvanMurzak.ReflectorNet.Convertor;
+using com.IvanMurzak.ReflectorNet.Model;
 using UnityEngine;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
@@ -22,6 +25,67 @@ namespace com.IvanMurzak.McpPlugin.Common.Reflection.Convertor
 {
     public partial class UnityArrayReflectionConvertor : ArrayReflectionConvertor
     {
+        public override object? Deserialize(
+            Reflector reflector,
+            SerializedMember data,
+            Type? fallbackType = null,
+            string? fallbackName = null,
+            int depth = 0,
+            StringBuilder? stringBuilder = null,
+            ILogger? logger = null)
+        {
+            if (fallbackType == null || !fallbackType.IsArray)
+                return base.Deserialize(reflector, data, fallbackType, fallbackName, depth, stringBuilder, logger);
+
+            var elementType = fallbackType.GetElementType();
+            if (elementType == null)
+                return base.Deserialize(reflector, data, fallbackType, fallbackName, depth, stringBuilder, logger);
+
+            if (data.valueJsonElement == null || data.valueJsonElement.Value.ValueKind != JsonValueKind.Array)
+                return base.Deserialize(reflector, data, fallbackType, fallbackName, depth, stringBuilder, logger);
+
+            var jsonArray = data.valueJsonElement.Value;
+            var length = jsonArray.GetArrayLength();
+            var array = Array.CreateInstance(elementType, length);
+
+            int index = 0;
+            foreach (var element in jsonArray.EnumerateArray())
+            {
+                SerializedMember? member = null;
+                if (element.ValueKind == JsonValueKind.Object &&
+                    (
+                        element.TryGetProperty(nameof(SerializedMember.typeName), out _) ||
+                        element.TryGetProperty(nameof(SerializedMember.fields), out _) ||
+                        element.TryGetProperty(nameof(SerializedMember.props), out _))
+                    )
+                {
+                    try
+                    {
+                        member = JsonSerializer.Deserialize<SerializedMember>(element.GetRawText());
+                        if (member != null && element.TryGetProperty(nameof(SerializedMember.valueJsonElement), out var valueProp))
+                        {
+                            member.valueJsonElement = valueProp;
+                        }
+                    }
+                    catch { }
+                }
+
+                if (member == null)
+                {
+                    member = new SerializedMember
+                    {
+                        valueJsonElement = element
+                    };
+                }
+
+                var value = reflector.Deserialize(member, elementType, null, depth + 1, stringBuilder, logger);
+                array.SetValue(value, index);
+                index++;
+            }
+
+            return array;
+        }
+
         public override IEnumerable<FieldInfo>? GetSerializableFields(Reflector reflector, Type objType, BindingFlags flags, ILogger? logger = null)
             => objType.GetFields(flags)
                 .Where(field => field.GetCustomAttribute<ObsoleteAttribute>() == null)
