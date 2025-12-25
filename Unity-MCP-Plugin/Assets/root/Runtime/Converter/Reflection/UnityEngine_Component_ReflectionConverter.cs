5384232813e7592974c86f6de2c49398941fc46d
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-12-24 16:42:10 -0800

    Refactor UnityEngine_Component_ReflectionConverter to inherit from UnityEngine_GenericComponent_ReflectionConverter and streamline property handling

--- Unity-MCP-Plugin/Assets/root/Runtime/Converter/Reflection/UnityEngine_Component_ReflectionConverter.cs
+++ Unity-MCP-Plugin/Assets/root/Runtime/Converter/Reflection/UnityEngine_Component_ReflectionConverter.cs
@@ -9,45 +9,10 @@
 */
 
 #nullable enable
-using System;
-using System.Collections.Generic;
-using com.IvanMurzak.ReflectorNet;
-using com.IvanMurzak.ReflectorNet.Model;
-using com.IvanMurzak.Unity.MCP.Runtime.Extensions;
-using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace com.IvanMurzak.Unity.MCP.Reflection.Converter
 {
-    public partial class UnityEngine_Component_ReflectionConverter : UnityEngine_Object_ReflectionConverter<UnityEngine.Component>
+    public partial class UnityEngine_Component_ReflectionConverter : UnityEngine_GenericComponent_ReflectionConverter<UnityEngine.Component>
     {
-        public override bool AllowSetValue => false;
-
-        protected override IEnumerable<string> GetIgnoredProperties()
-        {
-            foreach (var property in base.GetIgnoredProperties())
-                yield return property;
-
-            yield return nameof(UnityEngine.Component.gameObject);
-            yield return nameof(UnityEngine.Component.transform);
-#if UNITY_6000_3_OR_NEWER
-            yield return nameof(UnityEngine.Component.transformHandle);
-#endif
-        }
-        protected override object? DeserializeValueAsJsonElement(
-            Reflector reflector,
-            SerializedMember data,
-            Type type,
-            int depth = 0,
-            Logs? logs = null,
-            ILogger? logger = null)
-        {
-            return data.valueJsonElement
-                .ToObjectRef(
-                    reflector: reflector,
-                    depth: depth,
-                    logs: logs,
-                    logger: logger)
-                .FindObject() as UnityEngine.Component;
-        }
     }
 }
--- Unity-MCP-Plugin/Assets/root/Runtime/Converter/Reflection/UnityEngine_GenericComponent_ReflectionConverter.cs
+++ Unity-MCP-Plugin/Assets/root/Runtime/Converter/Reflection/UnityEngine_GenericComponent_ReflectionConverter.cs
@@ -10,10 +10,45 @@
 
 #nullable enable
 
+using System;
+using System.Collections.Generic;
+using com.IvanMurzak.ReflectorNet;
+using com.IvanMurzak.ReflectorNet.Model;
+using com.IvanMurzak.Unity.MCP.Runtime.Extensions;
+using Microsoft.Extensions.Logging;
+
 namespace com.IvanMurzak.Unity.MCP.Reflection.Converter
 {
     public abstract class UnityEngine_GenericComponent_ReflectionConverter<T> : UnityEngine_Object_ReflectionConverter<T> where T : UnityEngine.Component
     {
         public override bool AllowSetValue => false;
+
+        protected override IEnumerable<string> GetIgnoredProperties()
+        {
+            foreach (var property in base.GetIgnoredProperties())
+                yield return property;
+
+            yield return nameof(UnityEngine.Component.gameObject);
+            yield return nameof(UnityEngine.Component.transform);
+#if UNITY_6000_3_OR_NEWER
+            yield return nameof(UnityEngine.Component.transformHandle);
+#endif
+        }
+        protected override object? DeserializeValueAsJsonElement(
+            Reflector reflector,
+            SerializedMember data,
+            Type type,
+            int depth = 0,
+            Logs? logs = null,
+            ILogger? logger = null)
+        {
+            return data.valueJsonElement
+                .ToObjectRef(
+                    reflector: reflector,
+                    depth: depth,
+                    logs: logs,
+                    logger: logger)
+                .FindObject() as UnityEngine.Component;
+        }
     }
 }
